2d95122e78857443e101c0b4a3b8c0c091adf09d
Ian Agpawa <agpawaji@gmail.com>
2016-10-01 22:00:03 -0400

    need to paginate, mongo

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,21 @@ var mLab = "mongodb://localhost:27017/imageSearch"
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Search', author: "Ian Agpawa" });
+
 });
 
 
 //search for
+
+
 router.get("/:term", function (req, res, next) {
 
   var params = req.params.term;
-  console.log(params)
+  var time = new Date().toString();
 
   var searching = params;
   var base = "https://www.googleapis.com/customsearch/v1?key="
-
+  var keyCX = 
   var q = '&q='+ searching +'&searchType=image'
   var url = base + keyCX + q
 
@@ -49,67 +52,28 @@ router.get("/:term", function (req, res, next) {
   })
 
 
-
-/*
-title: items[i].title
-image url : items[i].link,
-alt text : items[i].snippet,
-page urls : items[i].image.contextLink
-
-
-
-url
-snippet
-thumbnail
-context
-
-
-
-
-*/
-
-
-
-/*
-  function getImgs(link) {
-    http.get(link, function(response){
-      response.on("data", function(chunk){
-        //do something with chunkData
-
-      });
-      response.on("end", function(){
-        //do somehing at the end, i.e. console.log(body), after adding to it
-
-      });
-    }).on('error', function(e){
-      console.log("ERROR: " + e.message)
-    }) // https
-  } //getImgs
-
-*/
-
-/*
-DON"T NEED YET
   //This is for the database
   mongo.connect(mLab, function (err, db){
     if (err) throw err;
-    console.log("connected, getting search term")
+    console.log("connected, mongo")
 
     var collection = db.collection("queries");
     var local = req.get("host");
 
 
+
     //enter search query into database
     var query = function (db, callback){
       var searchObj = {
-        search: params
+        search: params,
+        timeStamp: time
       } //searchObj
 
       collection.insert(searchObj);
 
       res.json({
         ///////////////////////////////////////////put output here
-        "searhced for: ": params
+
       })
     }//query
 
@@ -122,7 +86,7 @@ DON"T NEED YET
 
   }) // mongo connect
 
-  */
+
 
 
 
@@ -131,5 +95,7 @@ DON"T NEED YET
 
 
 }) //router.get
+
+
 
 module.exports = router;
